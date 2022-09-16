cc5f0b1ce9b1e0286cdc57f508f7897d87a04316
remi59800 <remimenart@gmail.com>
2022-09-16 17:15:21 +0200

    nav-content-okay

--- src/pages/SearchMovie.js
+++ src/pages/SearchMovie.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const SearchMovie = () => {
   return (
     <div>
+      <Header />
       <h2>SEARCH MOVIE</h2>
     </div>
   );
