2fd3bf4f0ecc583ba40316ea4c874453d41dd081
remi59800 <remimenart@gmail.com>
2022-09-19 15:31:10 +0200

    useeffect-api-ok-component-card-created

--- src/pages/SearchMovie.js
+++ src/pages/SearchMovie.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import Card from '../components/Card';
 
 const SearchMovie = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=2f29d9bc9f76a597232a8a514e956b12&query=code&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, [moviesData]);
+
   return (
     <div>
       <Header />
-      <h2>SEARCH MOVIE</h2>
+      <div className='form-container'>
+        <form>
+          <input
+            type='text'
+            placeholder="Entrez le titre d'un film"
+            id='search-input'
+          />
+          <input type='submit' value='Rechercher' />
+        </form>
+      </div>
+      <div className='result'>
+        {moviesData.map((movie) => {
+          return <Card />;
+        })}
+      </div>
     </div>
   );
 };
