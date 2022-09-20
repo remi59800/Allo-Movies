058c69f8e81fabb09e16e060fc755f6ae0b22caa
remi59800 <remimenart@gmail.com>
2022-09-20 11:42:06 +0200

    content-page-about-okay

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,32 @@ const About = () => {
   return (
     <div>
       <Header />
-      <h2>ABOUT</h2>
+      <h2>
+        Kult Film Club, l'alliance d'une passion pour le cinéma et le
+        développement web !{' '}
+      </h2>
+      <p>
+        Je suis Rémi, grand amateur de cinéma et néo-développeur web ! Après
+        plusieurs années de carrière dans la communication, j'ai beaucoup
+        réfléchi à ma carrière et mon avenir professionnel, et après moultes
+        réflexions, je décide de me lancer dans le développement web. Même si
+        tout l'aspect technique me faisait un peu peur au début, je découvre que
+        plein de monde y arrive, sans forcément sortir d'un BAC + 5 en
+        informatique. Il suffit simplement de s'en donner les moyens et de
+        travailler pour y arriver ! Je me lance donc dans une formation
+        intensive de cinq mois à la Wild Code School, où j'étudie d'abord le
+        HTML/CSS, Javascript, ReactJS, puis le back-end avec NodeJS, Express,
+        les bases de données en MySQL… Comme pour apprendre une langue
+        étrangère, apprendre le développement web prend du temps, il faut lire,
+        intégrer, pratiquer. Je me suis dit : pourquoi ne pas allier cette
+        nouvelle activité avec une autre passion, le cinéma ? C'est sur ce
+        constat qu'est né Kult Film Club, qui me permet de travailler mon code
+        et de rédiger des critiques de films que j'aime en essayant d'optimiser
+        également le référencement, et bien sûr de vous faire aimer ces films
+        aussi ! J'espère que la visite du site Kult Film Club vous plaira, ainsi
+        que la lecture de mes critiques ! N'hésitez pas à m'envoyer vos retours
+        par mail sur la page contact.{' '}
+      </p>
     </div>
   );
 };
