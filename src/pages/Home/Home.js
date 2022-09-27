a814d72bd33d141766244f12574d2d596a1007b0
remi59800 <remimenart@gmail.com>
2022-09-27 11:55:52 +0200

    added-page-search-movie

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import SearchMovie from '../../components/SearchMovie/SearchMovie';
+import InputSearch from '../../components/InputSearch/InputSearch';
+import SearchButton from '../../components/SearchButton/SearchButton';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// import SearchMovie from '../../components/SearchMovie/SearchMovie';
 
 const Home = () => {
+  const searchInputRef = useRef();
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    const searchResult = searchInputRef.current.value;
+    navigate('/recherche-films', { state: { searchResult: searchResult } });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div>
-      <SearchMovie />
+      {/* <SearchMovie /> */}
+      <InputSearch ref={searchInputRef} onKeyDown={handleKeyDown} />
+      <SearchButton onClick={handleSearch} />
     </div>
   );
 };
---
+++ src/pages/MovieSearch/MovieSearch.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import Card from '../../components/Cards/Cards';
+import InputSearch from '../../components/InputSearch/InputSearch';
+import SearchButton from '../../components/SearchButton/SearchButton';
+
+const MovieSearch = () => {
+  const [moviesData, setMoviesData] = useState([]);
+  const [searchMovie, setSearchMovie] = useState('');
+  const { state } = useLocation();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=2f29d9bc9f76a597232a8a514e956b12&query=${state.searchResult}&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, []);
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setSearchMovie(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    return await axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=2f29d9bc9f76a597232a8a514e956b12&query=${searchMovie}&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  };
+
+  return (
+    <div>
+      <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder={state.searchResult}
+            id='search-input'
+            value={searchMovie}
+            onChange={handleOnChange}
+          />
+          <input type='submit' value='Rechercher' />
+        </form>
+      </div>
+      <div className='result'>
+        {moviesData.slice(0, 12).map((movie) => {
+          return <Card movie={movie} key={movie.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MovieSearch;
