28c4ad7ba4ad0d9bd0bda19d13bbcd87f60b5485
remi59800 <remimenart@gmail.com>
2022-10-06 16:45:56 +0200

    added-random-image-movie

--- src/components/MoviesCards/MoviesCards.js
+++ src/components/MoviesCards/MoviesCards.js
@@ -11,21 +11,28 @@ const MoviesCards = ({ movie }) => {
     <div className='movies-cards'>
       <div className='poster-film'>
         <img
-          src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
+          src={
+            movie.poster_path
+              ? 'https://image.tmdb.org/t/p/original' + movie.poster_path
+              : './image-movie.png'
+          }
           alt={`Affiche ${movie.title}`}
         />
       </div>
       <div className='first-infos'>
-        <h2>{movie.title}</h2>
+        <h2>{movie ? movie.title : ''}</h2>
 
         <div className='date-and-note'>
-          <h4>Sorti le {dateFormater(movie.release_date) + ' •'}</h4>
           <h4>
-            &nbsp;Note : {movie.vote_average.toFixed(1)}/10 <span>⭐</span>
+            Sorti le {movie ? dateFormater(movie.release_date) + ' •' : ''}
+          </h4>
+          <h4>
+            &nbsp;{movie ? movie.vote_average.toFixed(1) : ''}/10{' '}
+            <span>⭐</span>
           </h4>
         </div>
         <div className='synopsis'>
-          <p>{movie.overview}</p>
+          <p>{movie ? movie.overview : ''}</p>
         </div>
         <div className='btn-more-infos'>
           <Link to={`/film/${movie.id}`}>
