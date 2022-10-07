9de24e6d8348f7f0a598689715dc700acbcadcb1
remi59800 <remimenart@gmail.com>
2022-10-08 00:28:36 +0200

    movies-recommandations-content-okay

--- src/components/RecoCards/RecoCards.js
+++ src/components/RecoCards/RecoCards.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const RecoCards = ({ reco }) => {
   return (
-    <div className='reco-cards'>
-      <div className='poster-film'>
-        <img
-          src={'https://image.tmdb.org/t/p/original' + reco.backdrop_path}
-          alt={`Affiche ${reco.title}`}
-        />
-      </div>
-      <div className='movie-title'>
-        <h4>{reco.title}</h4>
-      </div>
+    <div className='reco-card'>
+      <img
+        src={
+          reco
+            ? 'https://image.tmdb.org/t/p/original' + reco.backdrop_path
+            : './image-movie.png'
+        }
+        alt={`Affiche ${reco.title}`}
+      />
+
+      <h4>{reco.title}</h4>
     </div>
   );
 };
