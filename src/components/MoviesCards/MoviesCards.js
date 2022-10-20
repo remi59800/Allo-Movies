7428d3d84224ad6d0f85fdab5d06f5fa190362ab
remi59800 <remimenart@gmail.com>
2022-10-20 20:44:24 +0200

    added-buttons-pagination

--- src/components/MoviesCards/MoviesCards.js
+++ src/components/MoviesCards/MoviesCards.js
@@ -9,36 +9,38 @@ const MoviesCards = ({ movie }) => {
 
   return (
     <div className='movies-cards'>
-      <div className='poster-film'>
-        <img
-          src={
-            movie.poster_path
-              ? 'https://image.tmdb.org/t/p/original' + movie.poster_path
-              : './movie-bg.png'
-          }
-          alt={`Affiche ${movie.title}`}
-        />
-      </div>
+      <Link to={`/film/${movie.id}`}>
+        <div className='poster-film'>
+          <img
+            src={
+              movie.poster_path
+                ? 'https://image.tmdb.org/t/p/original' + movie.poster_path
+                : './movie-bg.png'
+            }
+            alt={`Affiche ${movie.title}`}
+          />
+        </div>
+      </Link>
       <div className='first-infos'>
-        <h2>{movie ? movie.title : ''}</h2>
-
-        <div className='date-and-note'>
-          <h4>
-            Sorti le {movie ? dateFormater(movie.release_date) + ' •' : ''}
-          </h4>
-          <h4>
-            &nbsp;{movie ? movie.vote_average.toFixed(1) : ''}/10{' '}
-            <span>⭐</span>
-          </h4>
-        </div>
-        <div className='synopsis'>
-          <p>{movie ? movie.overview : ''}</p>
-        </div>
-        <div className='btn-more-infos'>
-          <Link to={`/film/${movie.id}`}>
-            <input type='submit' value="Plus d'infos" />
-          </Link>
-        </div>
+        <Link to={`/film/${movie.id}`}>
+          <h2>{movie ? movie.title : ''}</h2>
+        </Link>
+        <Link to={`/film/${movie.id}`}>
+          <div className='date-and-note'>
+            <h4>
+              Sorti le {movie ? dateFormater(movie.release_date) + ' •' : ''}
+            </h4>
+            <h4>
+              &nbsp;{movie ? movie.vote_average.toFixed(1) : ''}/10{' '}
+              <span>⭐</span>
+            </h4>
+          </div>
+        </Link>
+        <Link to={`/film/${movie.id}`}>
+          <div className='synopsis'>
+            <p>{movie ? movie.overview : ''}</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
