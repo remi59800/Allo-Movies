439e1c1e0f78f0695e3d294b67b2f6495b8f43d7
remi59800 <remimenart@gmail.com>
2023-05-10 23:10:01 +0200

    fixed async problem on background image

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Loader() {
+    return (
+        <div className="loader-container">
+            <div className="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
+        </div>   );
+}
+
+export default Loader;
--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -20,6 +20,8 @@ const MovieDetail = () => {
     const [recommendMovie, setRecommendMovie] = useState([]);
     const [iconName, setIconName] = useState('regular-heart');
     const [isStored, setIsStored] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     const {id} = useParams();
 
@@ -29,19 +31,17 @@ const MovieDetail = () => {
 
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true);
             const [movieDataResponse, creditsResponse, videoResponse, recommendationsResponse] = await Promise.all([
                 axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=2f29d9bc9f76a597232a8a514e956b12&language=fr-FR`),
                 axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=2f29d9bc9f76a597232a8a514e956b12&language=fr-FR`),
                 axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=2f29d9bc9f76a597232a8a514e956b12&language=fr-FR`),
                 axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=2f29d9bc9f76a597232a8a514e956b12&language=fr-FR&page=1`),
             ]);
-
             const filteredResults = videoResponse.data.results.filter(video => {
                 const name = video.name.toLowerCase();
                 return name.includes('bande') || name.includes('trailer') || name.includes('bande-annonce') || name.includes('annonce');
             });
-
-
             setMovieData(movieDataResponse.data);
             setMovieCast(creditsResponse.data.cast);
             setMovieDirector(creditsResponse.data.crew.find((dir) => dir.job === 'Director'));
@@ -52,17 +52,14 @@ const MovieDetail = () => {
             }
             setRecommendMovie(recommendationsResponse.data.results);
         };
-
+        setIsLoading(false);
         fetchData();
+        setIsLoading(false);
     }, [id]);
 
     const dateFormater = (date) => {
         let [yy, mm, dd] = date.split('-');
         return [dd, mm, yy].join('/');
-    };
-
-    const imgBackground = {
-        originalImage: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
     };
 
     const addStorage = () => {
@@ -114,9 +111,16 @@ const MovieDetail = () => {
         };
     }, []);
 
+    const imgMovie = {
+        originalImage: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
+    };
+
     const backgroundImage = useMemo(() => {
+        if (isLoading) {
+            return 'none';
+        }
         if (screenWidth > 550 && movieData.backdrop_path) {
-            return `url(${imgBackground.originalImage(movieData.backdrop_path)})`;
+            return `url(${imgMovie.originalImage(movieData.backdrop_path)})`;
         } else if (screenWidth > 550 && !movieData.backdrop_path) {
             return `url('/movie-bg.png')`;
         } else {
@@ -124,10 +128,24 @@ const MovieDetail = () => {
         }
     }, [screenWidth, movieData.backdrop_path]);
 
+
+    const backgroundImageMobile = useMemo(() => {
+        if (isLoading) {
+            return 'none';
+        } if (movieData.backdrop_path) {
+            return imgMovie.originalImage(movieData.backdrop_path);
+        } else if (!movieData.backdrop_path) {
+            return '/movie-bg.png';
+        } else {
+            return 'none'
+        }
+    }, [movieData.backdrop_path]);
+
+
     return <div className='details'>
         <img
             className="backdrop-film-mobile"
-            src={movieData.backdrop_path ? `https://image.tmdb.org/t/p/w500${movieData.backdrop_path}` : "/movie-bg.png"}
+            src={backgroundImageMobile}
             alt={`Affiche de ${movieData.title}`}
         />
         <div
@@ -140,7 +158,6 @@ const MovieDetail = () => {
 
             }}
         >
-
             <div className='movie-details'>
                 {movieData.poster_path && <img
                         className="poster-film"
